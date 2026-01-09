5901403e005518a148b6c2236fe0e36318e88b2d
im-lakshyaa <lakshya20042004@gmail.com>
2026-01-09 23:51:11 +0530

    auth done

--- hospital-management/backend/middleware/authMiddleware.js
+++ hospital-management/backend/middleware/authMiddleware.js
@@ -1,11 +1,30 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user";
 
-const authMiddleware =async (req,res,next)=>{
-    try{
-        let token;
-        if(
-            req.headers.authrization
-        )
+const authMiddleware = async (req, res, next) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.header.authorization.split(" ")[1];
     }
-}
+    if (!token) {
+      return res.status(401).json({ message: "Not authorized, no token" });
+    }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      next();
+    
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
+export default authMiddleware;
